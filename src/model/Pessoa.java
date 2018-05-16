8b0c0884e4373288dc8855f4336e6687d5a138cb
eduardo.duarte <eduardo.duarte@UNI-MKT-CHF9LZ1.grupouniesp.com.br>
2018-05-16 11:49:05 -0300

    Alteração geral nos arquivos.

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -1,11 +1,12 @@
 package model;
+
 public class Pessoa {
-	
+
 	public static void falar() {
-		System.out.println("A pessoa esta falando");
+		System.out.println("A pessoa está falando");
 	}
-	
+
 	public String comer(String comida) {
-		return "A pessoa esta comendo "+comida;
+		return "A pessoa está comendo " + comida;
 	}
 }
--- src/view/Executa.java
+++ src/view/Executa.java
@@ -1,13 +1,13 @@
-package run;
+package view;
 
 import model.Pessoa;
 
 public class Executa {
 	public static void main(String[] args) {
-//		Nao foi necessario instanciar o objeto para chamar o metodo.
+		// Não foi necessário instanciar o objeto para chamar o método.
 		Pessoa.falar();
-		
-//		Agora instaciado, pois o outro metodo e' Void.
+
+		// Agora instaciado, pois o outro metodo é Void.
 		Pessoa pessoa = new Pessoa();
 		System.out.println(pessoa.comer("pizza"));
 	}
